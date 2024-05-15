4021606e735870ecd9fcd0edc264a5e85fad4b2e
rubenbot77 <rubenbot77@hotmail.com>
2024-05-15 18:15:48 -0500

    Toggle styles, new gradients config

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,11 @@ export const Header = () => {
       <hr className='bg-black h-[1.5px] mb-5' />
       <div className='flex justify-between'>
         <p>Dark Mode</p>
-        <input type='checkbox' />
+        <label htmlFor='darkmode' className='bg-Toggle w-12 h-6 rounded-full cursor-pointer p-[2px] relative'>
+          <input id='darkmode' type='checkbox' className='sr-only peer' />
+          <div className='w-12 h-6 rounded-full cursor-pointer absolute top-0 left-0 peer-checked:bg-Toggle-Gradient' />
+          <div className='w-5 h-5 bg-Light-Grayish-Blue rounded-full peer-checked:translate-x-6  peer-checked:bg-Very-Dark-Blue transition-all' />
+        </label>
       </div>
     </header>
   )
