7e7ba163a30a89b47e3d55165690c497f6568aed
rubenbot77 <rubenbot77@hotmail.com>
2024-05-16 14:08:42 -0500

    dark mode config, styles and logic, on color text of followers and dark mode text and change in switch position ligh on right dark on left

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,31 @@
+import { useState, useEffect } from 'react'
+
 export const Header = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const handleClick = () => {
+    setDarkMode(!darkMode)
+  }
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkMode])
+
   return (
-    <header className='bg-Very-Pale-Blue h-56 rounded-b-3xl pt-8 px-6'>
-      <h1 className='text-Very-Dark-Blue text-2xl font-bold mb-1'>Social Media Dashboard</h1>
-      <p className='text-Dark-Grayish-Blue font-boldp mb-6'>Total Follower: 23,004</p>
-      <hr className='bg-black h-[1.5px] mb-5' />
+    <header className='bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-56 rounded-b-3xl pt-8 px-6'>
+      <h1 className='text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-1'>Social Media Dashboard</h1>
+      <p className='text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-6'>Total Follower: 23,004</p>
+      <hr className='bg-Dark-Grayish-Blue h-[1px] mb-5 border-none' />
       <div className='flex justify-between'>
-        <p>Dark Mode</p>
+        <p className='text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold'>Dark Mode</p>
         <label htmlFor='darkmode' className='bg-Toggle w-12 h-6 rounded-full cursor-pointer p-[2px] relative'>
-          <input id='darkmode' type='checkbox' className='sr-only peer' />
+          <input onClick={handleClick} id='darkmode' type='checkbox' className='sr-only peer' />
           <div className='w-full h-full rounded-full cursor-pointer absolute top-0 left-0 peer-checked:bg-Toggle-Gradient' />
-          <div className='w-5 h-5 bg-Light-Grayish-Blue rounded-full peer-checked:translate-x-6  peer-checked:bg-Very-Dark-Blue transition-all' />
+          <div className='w-5 h-5 bg-Light-Grayish-Blue rounded-full translate-x-6 peer-checked:translate-x-0  peer-checked:bg-Very-Dark-Blue transition-all' />
         </label>
       </div>
     </header>
